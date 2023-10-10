4324cc05ac7f196ce0d0a020d033257a2dff9827
as <as.com>
2023-10-11 02:39:02 +0300

    deepspeech works

--- hw_asr/model/deep_speech.py
+++ hw_asr/model/deep_speech.py
@@ -10,23 +10,20 @@ rnn_types = {
 }
 
 class RNNBatchNorm(nn.Module):
-    def __init__(self, input_size, hidden_size, bidirectional, rnn_type, *args, **kwargs) -> None:
+    def __init__(self, input_size, hidden_size, bidirectional, rnn_type, use_norm=False, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
-        self.rnn = rnn_type(input_size=input_size, hidden_size=hidden_size, bidirectional=bidirectional)
-        self.batch_norm = nn.BatchNorm2d(num_features=1)
+        self.rnn = rnn_type(input_size=input_size, hidden_size=hidden_size, bidirectional=bidirectional, batch_first=True)
+        self.layer_norm = nn.LayerNorm(2 * hidden_size) if use_norm else nn.Identity()
     
     def forward(self, args):
         x = args[0]
         h = args[1]
+        x = self.layer_norm(x)
         x, h = self.rnn(x, h)
-        x = x.view(x.size(0), x.size(1), 2, -1)
-        x = x.sum(2)
-        x = x.view(x.size(0), x.size(1), -1)
-        x = self.batch_norm(x.unsqueeze(0).transpose(0, 1))
-        return x.squeeze(1), h
+        return x, h
 
 class DeepSpeech(BaseModel):
-    def __init__(self, n_feats, n_class, rnn_type='gru', rnn_hidden=1024, bidirectional=True, num_conv_layers=2, num_rnn_layers=5, **batch):
+    def __init__(self, n_feats, n_class, rnn_type='lstm', rnn_hidden=1024, bidirectional=True, num_conv_layers=2, num_rnn_layers=5, **batch):
         super().__init__(n_feats, n_class, **batch)
         assert 3 >= num_conv_layers > 0
         assert num_rnn_layers > 0
@@ -35,22 +32,22 @@ class DeepSpeech(BaseModel):
         self.conv_layers = nn.Sequential()
         conv1 = nn.Sequential(
             nn.Conv2d(padding=(20, 5), kernel_size=(41, 11), in_channels=1, out_channels=32, stride=(2, 2)),
-            nn.BatchNorm2d(num_features=32),
-            nn.LeakyReLU(),
+            nn.Hardtanh(0, 20, inplace=True),
+            nn.BatchNorm2d(num_features=32)
         )
         self.conv_layers.add_module("conv1", conv1)
         if num_conv_layers == 2:
             conv2 = nn.Sequential(
                 nn.Conv2d(padding=(10, 5), kernel_size=(21, 11), in_channels=32, out_channels=32, stride=(2, 1)),
+                nn.Hardtanh(0, 20, inplace=True),
                 nn.BatchNorm2d(num_features=32),
-                nn.LeakyReLU(),
             )
             self.conv_layers.add_module("conv2", conv2)
         if num_conv_layers == 3:
             conv3 = nn.Sequential(
                 nn.Conv2d(padding=(10, 5), kernel_size=(21, 11), in_channels=32, out_channels=96, stride=(2, 1)),
+                nn.Hardtanh(0, 20, inplace=True),
                 nn.BatchNorm2d(num_features=96),
-                nn.LeakyReLU(),
             )
             self.conv_layers.add_module("conv3", conv3)
         
@@ -72,7 +69,7 @@ class DeepSpeech(BaseModel):
                 self.all_rnn.add_module(f"rnn_0", rnn_0)
             else:
                 rnn_i = nn.Sequential(
-                    RNNBatchNorm(input_size=rnn_hidden, hidden_size=rnn_hidden, rnn_type=rnn_type, bidirectional=bidirectional),
+                    RNNBatchNorm(input_size=rnn_hidden * 2, hidden_size=rnn_hidden, rnn_type=rnn_type, bidirectional=bidirectional, use_norm=True),
                 )
                 self.all_rnn.add_module(f"rnn_{i}", rnn_i)
         
@@ -81,11 +78,11 @@ class DeepSpeech(BaseModel):
 
         #     )
         self.look_ahead = nn.Identity()
-        self.fc = nn.Linear(in_features=rnn_hidden, out_features=n_class)
+        self.fc = nn.Linear(in_features=rnn_hidden * 2, out_features=n_class)
 
 
     def forward(self, spectrogram, **batch):
-        x = self.conv_layers(spectrogram.unsqueeze(0).transpose(2, 3).transpose(0, 1))
+        x = self.conv_layers(spectrogram.unsqueeze(1).transpose(2, 3))
         x = x.view(x.shape[0], -1, self.rnn_input_size)
         x, _ = self.all_rnn((x, None))
         x = self.look_ahead(x)
