f7d69e45fb1e0357bebfeda3438b9155fd096f30
as <as.com>
2023-10-20 17:02:20 +0300

    fix

--- hw_asr/text_encoder/ctc_char_text_encoder.py
+++ hw_asr/text_encoder/ctc_char_text_encoder.py
@@ -53,8 +53,8 @@ class CTCCharTextEncoder(CharTextEncoder):
         if len(probs.shape) == 2:
             probs = probs.unsqueeze(0)
         
-        if len(probs_length.shape) == 1:
-            probs_length = probs_length.unsqueeze(0)
+        print(probs.shape)
+        print(probs_length)
             
         logits_list = np.array([probs[i][:probs_length[i]].detach().cpu().numpy() for i in range(probs_length.shape[0])])
 
