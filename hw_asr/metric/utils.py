80e354afa5f19076c7dd6e11890d88ac9dc06b70
as <as.com>
2023-11-10 20:32:26 +0300

    pizdets

--- hw_asr/metric/utils.py
+++ hw_asr/metric/utils.py
@@ -1,5 +1,5 @@
 import editdistance
-import numpy as np
+import torch
 # Don't forget to support cases when target_text == ''
 
 def calc_cer(target_text, predicted_text) -> float:
@@ -15,9 +15,9 @@ def calc_wer(target_text, predicted_text) -> float:
     predicted_text_splitted = predicted_text.split()
     return editdistance.eval(target_text_splitted, predicted_text_splitted) / len(target_text_splitted)
 
-def calc_snr(est, target):
-    return 20 * np.log10(np.linalg.norm(target) / (np.linalg.norm(target - est) + 1e-6) + 1e-6)
+# def calc_snr(est, target):
+#     return 20 * np.log10(np.linalg.norm(target) / (np.linalg.norm(target - est) + 1e-6) + 1e-6)
 
 def calc_si_sdr(est, target):
-    alpha = (target * est).sum() / np.linalg.norm(target)**2
-    return 20 * np.log10(np.linalg.norm(alpha * target) / (np.linalg.norm(alpha * target - est) + 1e-6) + 1e-6)
+    alpha = (target * est).sum() / torch.linalg.norm(target)**2
+    return 20 * torch.log10(torch.linalg.norm(alpha * target) / (torch.linalg.norm(alpha * target - est) + 1e-6) + 1e-6)
