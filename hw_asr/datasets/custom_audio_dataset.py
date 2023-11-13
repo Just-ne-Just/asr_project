2acfd307a34db34a3bb6a393c6b04576f61581d7
as <as.com>
2023-11-13 13:03:45 +0300

    fix

--- hw_asr/datasets/custom_audio_dataset.py
+++ hw_asr/datasets/custom_audio_dataset.py
@@ -12,8 +12,8 @@ class CustomAudioDataset(BaseDataset):
     def __init__(self, data, *args, **kwargs):
         index = data
         for entry in data:
-            entry["mix"] = str(Path(entry["mix"]).absolute().resolve())
-            entry["reference"] = str(Path(entry["reference"]).absolute().resolve())
-            entry["target"] = str(Path(entry["target"]).absolute().resolve())
+            entry["mix_path"] = str(Path(entry["mix_path"]).absolute().resolve())
+            entry["reference_path"] = str(Path(entry["reference_path"]).absolute().resolve())
+            entry["target_path"] = str(Path(entry["target_path"]).absolute().resolve())
 
         super().__init__(index, *args, **kwargs)
