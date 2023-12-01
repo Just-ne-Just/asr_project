b8e7ad213ebb9176457bb86d3fea1aa2ae19568c
Just-ne-Just <45533197+Just-ne-Just@users.noreply.github.com>
2023-12-02 01:06:37 +0300

    Update download.py

--- download.py
+++ download.py
@@ -11,7 +11,3 @@ gdown.download(url, output, quiet=False)
 url = "https://drive.usercontent.google.com/download?id=1SXOMnmVDEf9lua6qY8J8yW1-xbTDOND6&export=download&authuser=0&confirm=t&uuid=f073631e-f0d7-438e-ad87-44ec64585c45"
 output = "model_best.pth"
 gdown.download(url, output, quiet=False)
-
-url = "https://drive.usercontent.google.com/download?id=1MdWJSj80o40E9sU4wDw1pk7dFWjXnT1v&export=download&authuser=0&confirm=t&uuid=61f7318c-c95c-440c-8562-df9347dfc540"
-output = "config.json"
-gdown.download(url, output, quiet=False)
