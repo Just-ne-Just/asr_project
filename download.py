7237c310087bf0d21156cf0dcc26f7efdf6b479e
Just-ne-Just <45533197+Just-ne-Just@users.noreply.github.com>
2023-12-21 14:09:13 +0300

    Update download.py

--- download.py
+++ download.py
@@ -1,5 +1,5 @@
 import gdown
 
-url = "https://drive.usercontent.google.com/download?id=1d0SfbGkhNs7Xt6qIuwmX3NlE_zW8xp2K&export=download&authuser=2&confirm=t&uuid=b192e63b-7b48-4db4-a39d-bf1f8a2693a3"
+url = "https://drive.usercontent.google.com/download?id=1d0SfbGkhNs7Xt6qIuwmX3NlE_zW8xp2K&export=download&authuser=2&confirm=t&uuid=5dce46a0-c18d-4fc5-8f1d-0511fda9802d"
 output = "model_best.pth"
 gdown.download(url, output, quiet=False)
